50380a0597a39e911de2bc0f51a064df857e73c9
yassineam31 <yassineam31@gmail.com>
2025-05-12 12:37:49 +0100

    h

---
+++ index.js
@@ -0,0 +1,8 @@
+let btn=document.querySelector('button');
+btn.addEventListener('click',()=>{
+    swal.fire({
+        title:"mytitle",
+        text:"yyyyyyyyyyy",
+        icon:"success"
+    })
+})
